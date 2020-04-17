d29e58dea427810181a928d8b7a6ce1ab701d114
JeGarcia24 <jeralyn.garcia@yahoo.com>
2020-04-17 08:10:39 +0800

    Updates

---
+++ precache-manifest.9ce232ec37ea3b2b3c47c3fa16855d00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b27bbf7625d03f3dc7a98df79ac4720",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "fcefe2aded33f6edad45",
+    "url": "/calculator/static/css/2.81e2c8a5.chunk.css"
+  },
+  {
+    "revision": "04b6c23a01f6199e9952",
+    "url": "/calculator/static/css/main.1902212b.chunk.css"
+  },
+  {
+    "revision": "fcefe2aded33f6edad45",
+    "url": "/calculator/static/js/2.941254e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.941254e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04b6c23a01f6199e9952",
+    "url": "/calculator/static/js/main.29b0e1a8.chunk.js"
+  },
+  {
+    "revision": "552c96952084d63edc80",
+    "url": "/calculator/static/js/runtime-main.29434a71.js"
+  }
+]);
